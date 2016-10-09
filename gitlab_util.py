2ca62a02b715c077c0e51472de5bf432de7c0716
王金泽 <wangjz@aigongzuo.com>
2016-10-09 18:39:57 +0800

    add logging feature
    Signed-off-by: 王金泽 <wangjz@aigongzuo.com>

--- gitlab_util.py
+++ gitlab_util.py
@@ -1,6 +1,7 @@
 import requests
 import datetime
 import ConfigParser
+import logging
 
 
 def get_projects():
@@ -13,10 +14,7 @@ def get_projects():
         private_token=private_token
     )
     resp = requests.get(projects_url, params=data)
-    if resp.status_code == 200:
-        return resp.json()
-    else:
-        return str(resp.status_code) + ' ' + resp.text
+    return resp.status_code, resp.json(), resp.text
 
 
 def get_mergerequests(project_id, state=None):
@@ -32,29 +30,7 @@ def get_mergerequests(project_id, state=None):
         state=state
     )
     resp = requests.get('{0}/{1}/merge_requests'.format(projects_url, project_id), params=data)
-    if resp.status_code == 200:
-        return resp.json()
-    else:
-        return str(resp.status_code) + ' ' + resp.text
-
-
-def get_mergerequest_commits(project_id, mergerequest_id):
-    """Get commits of a merge request.
-
-    :param project_id: ID of the project
-    :param mergerequest_id: ID of the merge request
-    :return: numbers of specified merge request commits
-    """
-    data = dict(
-        private_token=private_token,
-    )
-    resp = requests.get('{0}/{1}/merge_request/{2}/commits'.format(projects_url, project_id, mergerequest_id),
-                        params=data)
-
-    if resp.status_code == 200:
-        return resp.json()
-    else:
-        return str(resp.status_code) + ' ' + resp.text
+    return resp.status_code, resp.json(), resp.text
 
 
 def get_mergerequest_commits(project_id, mergerequest_id):
@@ -117,34 +93,57 @@ def accept_mergerequest(project_id, mergerequest_id):
         return str(resp.status_code) + ' ' + resp.text
 
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s---%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    filename='gitlab_util.log',
+                    filemode='a')
+
+# Print log to console
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s---%(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+
+#*******************************   read configuration start ********************************
 config = ConfigParser.ConfigParser()
 config.read('gitlab_config.conf')
 
-basic_section = 'basic'
-gitlab_host = config.get(basic_section, 'gitlab_host').rstrip('/')
-private_token = config.get(basic_section, 'private_token')
-source_branch = config.get(basic_section, 'source_branch')
-target_branch = config.get(basic_section, 'target_branch')
+merge_type = 'dev-to-test'
+gitlab_host = config.get('common', 'gitlab_host').rstrip('/')
+private_token = config.get('common', 'private_token')
+source_branch = config.get(merge_type, 'source_branch')
+target_branch = config.get(merge_type, 'target_branch')
+project_list = config.get(merge_type, 'project_list').split(',')
+
+
+#*******************************   read configuration end ********************************
 
 projects_url = gitlab_host + '/api/v3/projects'
 
 project_id = 67
-project_name = 'agz-bussiness'
+project_name = 'agz-business'
 
 # Get all opened merge request for specific project
-merge_request_list = get_mergerequests(project_id, state='opened')
-if merge_request_list:
-    for i in merge_request_list:
-        if i['source_branch'] == source_branch and i['target_branch'] == target_branch and i[
-            'merge_status'] == 'can_be_merged':
-            print accept_mergerequest(project_id, i['id'])
-        else:
-            print 'skip merge due to merge status unchecked, project_name={0}'.format(project_name)
+status_code, mergerequest_list, err_msg = get_mergerequests(project_id, state='opened')
+if status_code == 200:
+    if mergerequest_list:
+        for i in mergerequest_list:
+            if i['source_branch'] == source_branch and i['target_branch'] == target_branch \
+                    and i['merge_status'] == 'can_be_merged':
+                print accept_mergerequest(project_id, i['id'])
+            else:
+                logging.info('skip merge due to merge status {0}, project_name={1}'\
+                             .format(i['merge_status'], project_name))
 
-else:
-    now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    data = create_mergerequest(project_id, source_branch, target_branch, title=project_name + '_' + now)
-    if data['merge_status'] == 'can_be_merged':
-        print accept_mergerequest(project_id, data['id'])
     else:
-        print data['merge_status']
+        now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        data = create_mergerequest(project_id, source_branch, target_branch, title=project_name + '_' + now)
+        if data['merge_status'] == 'can_be_merged':
+            print accept_mergerequest(project_id, data['id'])
+        else:
+            print data['merge_status']
+else:
+    logging.warning("Get merge request failed, project_name={0}, status_code={1}, err_msg={2}"\
+                    .format(project_name, str(status_code), err_msg))
--- t.py
+++ t.py
@@ -3,3 +3,13 @@ import datetime
 now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 print now
+
+
+
+def two_return_test():
+    a = 1
+    b = 2
+    return a,b
+
+c, d = two_return_test()
+print c, d
