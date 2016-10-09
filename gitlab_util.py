29b2049aad2348b54c751414365b5de056b65ac4
王金泽 <wangjz@aigongzuo.com>
2016-10-09 17:21:08 +0800

    refine gitlab_util
    Signed-off-by: 王金泽 <wangjz@aigongzuo.com>

--- gitlab_util.py
+++ gitlab_util.py
@@ -57,6 +57,25 @@ def get_mergerequest_commits(project_id, mergerequest_id):
         return str(resp.status_code) + ' ' + resp.text
 
 
+def get_mergerequest_commits(project_id, mergerequest_id):
+    """Get commits of a merge request.
+
+    :param project_id: ID of the project
+    :param mergerequest_id: ID of the merge request
+    :return: numbers of specified merge request commits
+    """
+    data = dict(
+        private_token=private_token,
+    )
+    resp = requests.get('{0}/{1}/merge_request/{2}/commits'.format(projects_url, project_id, mergerequest_id),
+                        params=data)
+
+    if resp.status_code == 200:
+        return resp.json()
+    else:
+        return str(resp.status_code) + ' ' + resp.text
+
+
 def create_mergerequest(project_id, source_branch, target_branch, title):
     """Create a new merge request.
 
@@ -78,21 +97,54 @@ def create_mergerequest(project_id, source_branch, target_branch, title):
     else:
         return str(resp.status_code) + ' ' + resp.text
 
+
+def accept_mergerequest(project_id, mergerequest_id):
+    """auto accept an existing merge request.
+
+    :param project_id: ID of the project originating the merge request
+    :param mergerequest_id: ID of the merge request to accept
+    :return: dict of the modified merge request
+    """
+    data = dict(
+        private_token=private_token,
+    )
+
+    resp = requests.put('{0}/{1}/merge_request/{2}/merge'.format(projects_url, project_id, mergerequest_id),
+                        params=data)
+    if resp.status_code == 200:
+        return resp.json()
+    else:
+        return str(resp.status_code) + ' ' + resp.text
+
+
 config = ConfigParser.ConfigParser()
 config.read('gitlab_config.conf')
 
-gitlab_host = config.get('basic', 'gitlab_host').rstrip('/')
-private_token = config.get('basic', 'private_token')
+basic_section = 'basic'
+gitlab_host = config.get(basic_section, 'gitlab_host').rstrip('/')
+private_token = config.get(basic_section, 'private_token')
+source_branch = config.get(basic_section, 'source_branch')
+target_branch = config.get(basic_section, 'target_branch')
 
-api_url = gitlab_host + '/api/v3'
-projects_url = api_url + "/projects"
+projects_url = gitlab_host + '/api/v3/projects'
 
 project_id = 67
-source_branch = 'dev'
-target_branch = 'test'
+project_name = 'agz-bussiness'
 
-#print get_mergerequests(67, state='opened')
-#print len(get_mergerequest_commits(67, 977))
-now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-print projects_url
-print create_mergerequest(project_id, source_branch, target_branch, title='agz-business-'+ now)
+# Get all opened merge request for specific project
+merge_request_list = get_mergerequests(project_id, state='opened')
+if merge_request_list:
+    for i in merge_request_list:
+        if i['source_branch'] == source_branch and i['target_branch'] == target_branch and i[
+            'merge_status'] == 'can_be_merged':
+            print accept_mergerequest(project_id, i['id'])
+        else:
+            print 'skip merge due to merge status unchecked, project_name={0}'.format(project_name)
+
+else:
+    now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    data = create_mergerequest(project_id, source_branch, target_branch, title=project_name + '_' + now)
+    if data['merge_status'] == 'can_be_merged':
+        print accept_mergerequest(project_id, data['id'])
+    else:
+        print data['merge_status']
