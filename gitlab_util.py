5ee787128f52a9927e8bc7dcedf2d48307f42ed8
王金泽 <wangjz@aigongzuo.com>
2016-10-11 15:28:20 +0800

    1111111111

--- gitlab_util.py
+++ gitlab_util.py
@@ -11,12 +11,15 @@ def get_projects():
 
         :return: list with the repo name, description, web url, ssh url
         """
-    data = dict(
+    params = dict(
         per_page=200,
         private_token=private_token
     )
-    resp = requests.get(projects_url, params=data)
-    return resp.status_code, resp.json(), resp.text
+    resp = requests.get(projects_url, params=params)
+    if resp.status_code == 200:
+        return resp.json()
+    else:
+        raise Exception(resp.status_code, resp.text)
 
 
 def get_mergerequests(project_id, state=None):
@@ -26,12 +29,12 @@ def get_mergerequests(project_id, state=None):
         :param state: Passes merge request state to filter them by it
         :return: List with all the merge requests
         """
-    data = dict(
+    params = dict(
         per_page=20,
         private_token=private_token,
         state=state
     )
-    resp = requests.get('{0}/{1}/merge_requests'.format(projects_url, project_id), params=data)
+    resp = requests.get('{0}/{1}/merge_requests'.format(projects_url, project_id), params=params)
     if resp.status_code == 200:
         return resp.json()
     else:
@@ -45,11 +48,11 @@ def get_mergerequest_commits(project_id, mergerequest_id):
     :param mergerequest_id: ID of the merge request
     :return: numbers of specified merge request commits
     """
-    data = dict(
+    params = dict(
         private_token=private_token,
     )
     resp = requests.get('{0}/{1}/merge_request/{2}/commits'.format(projects_url, project_id, mergerequest_id),
-                        params=data)
+                        params=params)
 
     if resp.status_code == 200:
         return resp.json()
@@ -66,18 +69,28 @@ def create_mergerequest(project_id, source_branch, target_branch, title):
     :param title: Title of the merge request
     :return: dict of the new merge request
     """
-    data = dict(
+    params = dict(
         private_token=private_token,
         source_branch=source_branch,
         target_branch=target_branch,
         title=title
     )
-    resp = requests.post('{0}/{1}/merge_requests'.format(projects_url, project_id), params=data)
-    if resp.status_code == 201:
-        logging.info('New merge request created, project={0}'.format(project_id))
-        return resp.json()
+
+    try:
+        resp = requests.post('{0}/{1}/merge_requests'.format(projects_url, project_id), params=params)
+        resp.raise_for_status()
+    except requests.RequestException as e:
+        print e
     else:
-        raise Exception('create merge request failed', str(resp.status_code), resp.text, title)
+        return resp.json()
+
+    # if resp.status_code == 201:
+    #     logging.info('Merge request created, title={0}'.format(title))
+    #     return resp.json()
+    # else:
+    #     logging.warning('create merge request failed', str(resp.status_code), resp.text, title)
+    #
+    #     raise Exception('create merge request failed', str(resp.status_code), resp.text, title)
 
 
 def accept_mergerequest(project_id, mergerequest_id):
@@ -87,17 +100,20 @@ def accept_mergerequest(project_id, mergerequest_id):
     :param mergerequest_id: ID of the merge request to accept
     :return: dict of the modified merge request
     """
-    data = dict(
+    params = dict(
         private_token=private_token,
     )
 
     resp = requests.put('{0}/{1}/merge_request/{2}/merge'.format(projects_url, project_id, mergerequest_id),
-                        params=data)
-    if resp.status_code == 200:
-        logging.info('merge request accepted, name={0}'.format(mergerequest_title))
-        return resp.json()
-    else:
-        raise Exception('accept merge request failed', str(resp.status_code), resp.text)
+                        params=params)
+    resp.raise_for_status()
+
+    return resp.json()
+    # if resp.status_code == 200:
+    #     logging.info('merge request accepted, name={0}'.format(mergerequest_title))
+    #     return resp.json()
+    # else:
+    #     raise Exception('accept merge request failed', str(resp.status_code), resp.text)
 
 
 logging.basicConfig(level=logging.DEBUG,
@@ -115,15 +131,24 @@ formatter = logging.Formatter('%(asctime)s %(filename)s [line:%(lineno)d] %(leve
 console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
 
+logging.info('\n\n')
+logging.info("Merge start")
+
 config = ConfigParser.ConfigParser()
 config.read('gitlab_util.conf')
 
-merge_type = 'dev-to-test'
+merge_branches = 'dev-to-test'
 gitlab_host = config.get('common', 'gitlab_host').rstrip('/')
 private_token = config.get('common', 'private_token')
-source_branch = config.get(merge_type, 'source_branch')
-target_branch = config.get(merge_type, 'target_branch')
-project_list = config.get(merge_type, 'project_list').split(',')
+source_branch = config.get(merge_branches, 'source_branch')
+target_branch = config.get(merge_branches, 'target_branch')
+project_list = config.get(merge_branches, 'project_list').split(',')
+
+logging.info("gitlab_host = " + gitlab_host)
+logging.info("merge_branches = " + merge_branches)
+logging.info("project_list = " + str(project_list))
+
+
 
 projects_url = gitlab_host + '/api/v3/projects'
 
@@ -140,7 +165,7 @@ mergerequest_to_be_merged = []
 for project_name in project_list:
     project_id = project_dict[project_name]
 
-    logging.info("Start process merge request for {0}".format(project_name))
+    logging.info("Start process merge request for project {0}".format(project_name))
 
     try:
         # Check if there is any legacy merge request
@@ -154,26 +179,29 @@ for project_name in project_list:
                     mergerequest_type = 'legacy'
                     logging.info('Legacy merge request found, name={0}, merge_status={1}'
                                  .format(mergerequest_title, mergerequest_status))
-                    mergerequest_to_be_merged.append(
-                        dict(project_id=project_id, mergerequest_title=mergerequest_title, mergerequest_id=mergerequest_id,
-                             mergerequest_status=mergerequest_status, mergerequest_type=mergerequest_type))
+
         # No legacy merge request, create new merge request
         else:
             cur_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
             mergerequest_title = project_name + '_' + cur_time
             data = create_mergerequest(project_id, source_branch, target_branch, title=mergerequest_title)
             mergerequest_id = data['id']
-            mergerequest_status = data['status']
+            mergerequest_status = data['merge_status']
             mergerequest_type = 'new'
-            mergerequest_to_be_merged.append(
-                dict(project_id=project_id, mergerequest_title=mergerequest_title, mergerequest_id=mergerequest_id,
-                     mergerequest_status=mergerequest_status, mergerequest_type=mergerequest_type))
+
+        mergerequest_to_be_merged.append(
+            dict(project_id=project_id, project_name=project_name, mergerequest_title=mergerequest_title,
+                 mergerequest_id=mergerequest_id, mergerequest_status=mergerequest_status,
+                 mergerequest_type=mergerequest_type))
     except Exception as e:
-        print sys.exc_info()
+        print e.message
+
+logging.debug(mergerequest_to_be_merged)
 
 # Process collected merge request
 for mr in mergerequest_to_be_merged:
-    project_id=mr['project_id']
+    project_id = mr['project_id']
+    project_name = mr['project_name']
     mergerequest_id = mr['mergerequest_id']
     mergerequest_title = mr['mergerequest_title']
     mergerequest_status = mr['mergerequest_status']
@@ -183,12 +211,21 @@ for mr in mergerequest_to_be_merged:
 
     if num_of_commits:
         if mergerequest_status == "cannot_be_merged":
-            result_table.add_row([project_name, mergerequest_title, mergerequest_type, num_of_commits, mergerequest_status])
+            result_table.add_row([project_name, mergerequest_title, mergerequest_type,
+                                  num_of_commits, mergerequest_status])
             continue
+
         accept_mergerequest(project_id, mergerequest_id)
+
         result_table.add_row([project_name, mergerequest_title, mergerequest_type, num_of_commits, "merged"])
+
     else:
-        logging.info('No commits for {0}, skipped auto accept'.format(mergerequest_title))
+        logging.info('No commits for {0}, auto merge skipped'.format(mergerequest_title))
         result_table.add_row([project_name, mergerequest_title, mergerequest_type, num_of_commits, 'skipped'])
 
-print result_table
+logging.info("Printing merge results")
+logging.info(result_table)
+logging.info("Merge end")
+
+print  result_table
+
