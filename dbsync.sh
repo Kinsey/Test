9f504a0c160205fe9aaa4187a8525cf7f09573ef
wangjz <wangjz@aigongzuo.com>
2016-12-06 18:31:57 +0800

    add db sync script
    Signed-off-by: wangjz <wangjz@aigongzuo.com>

---
+++ dbsync.sh
@@ -0,0 +1,189 @@
+#!/bin/bash
+
+sync_init () {
+  rm -rf /usr/local/ht_devops/dbsync/workspace/*
+
+  mysql_data_file=$bak_data/mysql-$bak_date.tar.gz
+  mongo_data_file=$bak_data/mongo-$bak_date.tar.gz
+
+  uncompress_file_to_workspace $mysql_data_file
+  uncompress_file_to_workspace $mongo_data_file
+
+  mongo_update_scripts=`find update_scripts -name '*.js' -printf '%f\n' | sort | xargs `
+
+  #sort scripts by date nested in filename, asending
+  mysql_update_scripts=`find update_scripts -name "*.sql" -printf "%f\n" | awk '{FS="_"; $0=$0;  print $NF"|"$0}' | sort | cut -d"|" -f2 | xargs`
+}
+
+
+uncompress_file_to_workspace () {
+  file_name=$1
+
+  tar -zxf $file_name -C $workspace
+
+  if [ $? -ne 0 ];then
+    echo "uncompress file $file_name failed"
+    exit -1
+  fi  
+}
+
+
+mongo_sync_start () {
+  mongo_port=27017
+  import_mongo_data
+
+  if [ -n "$mongo_update_scripts" ];then
+    exec_mongo_update_scripts
+  fi
+}
+
+
+import_mongo_data () {
+  echo -n "importing mongodb data..."
+  cmd="mongorestore -h $mongo_host:$mongo_port --batchSize=10 --quiet $workspace/$bak_date --drop"
+  `$cmd`
+
+  check_status
+}
+
+
+exec_mongo_update_scripts () {
+  for script in $mongo_update_scripts
+  do
+    echo -n "executing $script..."
+    cmd="mongo --quiet $mongo_host:$mongo_port/component update_scripts/$script"
+    `$cmd`
+    check_status
+  done
+}
+
+
+mysql_sync_start () {
+  mysql_user='root'
+  mysql_pwd='admin123'
+  mysql_cmd="mysql -h $mysql_host -u $mysql_user -p$mysql_pwd -e"
+
+  drop_mysql_databases
+  import_mysql_data
+
+  if [ -n "$mysql_update_scripts" ];then
+    exec_mysql_update_scripts
+  fi
+  
+}
+
+
+drop_mysql_databases () {
+  echo -n "dropping mysql databases..."
+
+  #clear all databases prefixed with agz_ and database app_platform_config
+  for db in `$mysql_cmd "show databases" | grep -E '(agz_|app_platform_config)' `
+  do
+         $mysql_cmd "drop database if exists $db;"
+  done
+  echo "ok"
+}
+
+
+import_mysql_data() {
+  echo -n "importing mysql data..."
+
+  sed -i '/GLOBAL.GTID_PURGED/d' $workspace/$bak_date.sql
+
+  cmd="$mysql_cmd 'source $workspace/$bak_date.sql'"
+
+  $mysql_cmd "source $workspace/$bak_date.sql"
+
+	check_status
+}
+
+
+exec_mysql_update_scripts () {
+  for script in $mysql_update_scripts
+  do
+    cmd="$mysql_cmd 'source update_scripts/$script'"
+     
+    echo -n "executing $script..."   
+    $mysql_cmd "source update_scripts/$script"
+    
+    check_status
+  done
+}
+
+
+print_sync_detail () {
+  echo "Please confirm following information: "
+  echo ""
+  echo "mysql_host: $mysql_host"
+  echo "mysql_update_scripts: $mysql_update_scripts"
+  echo ""
+  echo "mongo_host: $mongo_host"
+  echo "mongo_update_script: $mongo_update_scripts"
+  echo ""
+  echo "data_date=$bak_date"
+}
+
+prompt_for_confirmation () {
+	echo -n "Type 'continue' to proceed: "
+	read answer
+	if [ "$answer" != "continue" ];then
+ 		echo "exit due to user abort"
+		exit 1
+	fi
+  echo ""
+}
+
+check_status () {
+  if [ $? -eq 0 ];then
+    echo "ok"
+  else
+    echo "command \"$cmd\" failed to execute"
+    exit -1
+  fi    
+}
+
+
+option="${1}"
+case ${option} in
+   dev01) mysql_host="dev01.demo.com"
+          mongo_host="dev01.demo.com"
+      ;;
+   tev02) mysql_host="mysql02.demo.com"
+          mongo_host="mongo02.demo.com"
+      ;;
+   lb01)  mysql_host="mysql03.demo.com"
+          mongo_host="IM03.demo.com"
+      ;;
+   cp01)  mysql_host="mysql01.demo.com"
+          mongo_host="mongo01.demo.com"
+      ;;
+   cp02)  mysql_host="cp02.demo.com"
+          mongo_host="cp02.demo.com"
+      ;;
+   testjk)  mysql_host="testjk.demo.com"
+          mongo_host="testjk.demo.com"
+      ;;
+   *)
+      echo "usage: `basename ${0}` [dev01|tev02|lb01|cp01|cp02]"
+      exit 1
+      ;;
+esac
+
+bak_date=`date +%Y%m%d`
+
+ht_devops=/usr/local/ht_devops
+dbsync=$ht_devops/dbsync
+bak_data=$dbsync/bak_data
+workspace=$dbsync/workspace
+
+sync_init
+
+print_sync_detail
+prompt_for_confirmation
+
+mongo_sync_start
+mysql_sync_start
+
+# flush redis and restart mycat service
+python flush_service.py option="${1}"
+
---
+++ flush_service.py
@@ -0,0 +1,64 @@
+import ssh
+import sys
+import time
+
+
+def flush_redis(env):
+    myclient = ssh.SSHClient()
+    myclient.set_missing_host_key_policy(ssh.AutoAddPolicy())
+    myclient.connect(basic_info[env]['redis_host'], port=22, username="root", password=basic_info[env]['password'])
+    myclient.exec_command("service redis start")
+    time.sleep(2)
+    stdin, stdout, stderr = myclient.exec_command("redis-cli flushall")
+    print basic_info[env]['redis_host'] + ": flush redis..." + stdout.read().lower()
+
+
+def restart_mycat(env):
+    myclient = ssh.SSHClient()
+    myclient.set_missing_host_key_policy(ssh.AutoAddPolicy())
+    myclient.connect(basic_info[env]['mycat_host'], port=22, username="root", password=basic_info[env]['password'])
+    print "restart mycat..."
+    myclient.exec_command(basic_info[env]['mycat_restart_cmd'])
+    time.sleep(2)
+    stdin, stdout, stderr = myclient.exec_command(basic_info[env]['mycat_status_cmd'])
+    print basic_info[env]['mycat_host'] + ": " + stdout.read()
+
+basic_info = {'dev01': {'password':'111111',
+                         'redis_host':'dev01.demo.com',
+                         'mycat_host': "dev01.demo.com",
+                         'mycat_restart_cmd': "service mycat restart",
+                         'mycat_status_cmd': "service mycat status",
+                         },
+               'tev02': {'password':'hongtoo123!',
+                          'redis_host':'mycat02.demo.com',
+                          'mycat_host': "mycat02.demo.com",
+                          'mycat_restart_cmd': "/home/mycat/bin/mycat restart",
+                          'mycat_status_cmd': "/home/mycat/bin/mycat status",
+                         },
+                'cp01': {'password':'hongtoo123!',
+                          'redis_host':'mycat01.demo.com',
+                          'mycat_host': "mycat01.demo.com",
+                          'mycat_restart_cmd': "service mycat restart",
+                          'mycat_status_cmd': "service mycat status",
+                         },
+                'cp02': {'password':'111111',
+                          'redis_host':'cp02.demo.com',
+                          'mycat_host': "cp02.demo.com",
+                          'mycat_restart_cmd': "service mycat restart",
+                          'mycat_status_cmd': "service mycat status",
+                         },
+                'testjk': {'password':'hongtoo123!',
+                          'redis_host':'testjk.demo.com',
+                          'mycat_host': "testjk.demo.com",
+                          'mycat_restart_cmd': "service mycat restart",
+                          'mycat_status_cmd': "service mycat status",
+                         },
+              }
+
+if sys.argv[1] in basic_info.keys():
+    flush_redis(sys.argv[1])
+    restart_mycat(sys.argv[1])
+elif sys.argv[1] == 'lb01':
+    print "lb01 is not supported, please flush the services manually"
+else:
+    print "Unknown environment"
